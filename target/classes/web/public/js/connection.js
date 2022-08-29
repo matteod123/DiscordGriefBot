bc85a577dfc7a0e142c792da929f8228d558a04c
CorruptedBytes <52111292+CorruptedBytes@users.noreply.github.com>
2022-08-29 22:05:59 +0200

    Removed unnecessary stuff

--- target/classes/web/public/js/connection.js
+++
@@ -1,8 +0,0 @@
-const read = fetch("/websocket", { method: 'GET' }).then(response => response.text("Result"));
-init();
-
-function init() {
-    read.then((result) => {
-        startSocket(result.toString());
-    });
-}
